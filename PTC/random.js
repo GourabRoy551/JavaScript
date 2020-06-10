a72e6d3dd27718efde8a48c3ffd323dfb4b125d1
GourabRoy551 <gourabroysec553@gmail.com>
2020-06-10 20:18:42 +0600

    Again

--- PTC/random.js
+++ PTC/random.js
@@ -1,4 +1,27 @@
-var a = 5;
-var b = 7;
+// var a = 5;
+// var b = 7;
 
-console.log("Before swap: a=", a, "b=", b);
+// console.log("Before swap: a=", a, "b=", b);
+// document.getElementsByTagName('p')[1].innerHTML = "What does the Fox say?";
+
+// console.log(breed);
+// const breed = "Cocker Spaniel";
+
+// const person = {
+//     name: 'Dev',
+//     age: 23
+// };
+
+// Object.freeze(person);
+// person.age = 24;
+
+// console.log(person);
+
+const person = {
+    name: 'Dev',
+    age: 23
+};
+
+person.age = 24;
+
+console.log(person);
